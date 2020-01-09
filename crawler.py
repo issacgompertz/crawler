d02d216cb2508bd303f488ed5aab4e9ed1572145
root <weiwei.gong@bangcle.com>
2020-01-09 01:54:55 -0500

    Add sitemap

--- crawler.py
+++ crawler.py
@@ -1,19 +1,36 @@
 import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
+import  re
 
-def Download(url, num_retries = 2, user_agent = 'wswp'):
+def Download(url, num_retries = 2, user_agent = 'wswp', charset = 'utf-8'):
     print('Downloading: ',url)
     request = urllib.request.Request(url)
     request.add_header('User_agent', user_agent)
     try:
-        html = urllib.request.urlopen(url).read()
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset()
+        if not cs:
+            cs = charset
+        html = resp.read().decode(cs)
+        #html = urllib.request.urlopen(url).read()
     except (URLError, HTTPError, ContentTooShortError) as error:
         print('Downloading error: ', error.reason)
         html = None
         if num_retries > 0:
             if hasattr(error, 'code') and 500 <= error.code < 600:
                 return download(url, num_retries-1)
+   # print(html)
     return html
+
+def CrawlSitemap(url):
+    #Download the sitemap file
+    sitemap = Download(url)
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    print(links)
+    for link in links:
+        print(link)
+        html = Download(link)
+        print(html+'\n\n\n\n')
 
 def GetUrl():
     url_head = "https://"
@@ -28,8 +45,9 @@ def SaveHtml(FileName,FileContent):
 
 def main():
     url = GetUrl()
-    html = Download(url)
-    SaveHtml("bing",html)
-    print(html)
+   # html = Download(url)
+   # SaveHtml("bing",html)
+    CrawlSitemap(url)
+#    print(html)
 
 main()
--- demo.py
+++
@@ -1,3 +0,0 @@
-print("Hello crawl,python,git")
-
-
